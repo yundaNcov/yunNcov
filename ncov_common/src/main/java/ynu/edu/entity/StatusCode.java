309965d8eb1417c81b88a48b08529dbd9eeda908
duanran <214336861@qq.com>
2020-02-17 17:18:33 +0800

    The project framework，begin coding

---
+++ ncov_common/src/main/java/ynu/edu/entity/StatusCode.java
@@ -0,0 +1,8 @@
+package ynu.edu.entity;
+
+public class StatusCode {
+    public static final int OK = 20000;//操作成功
+    public static final int ERROR = 20001;//操作失败
+    public static final int LOGINERROR =20002;//用户名或密码错误
+    public static final int ACCESSERROR =20003;//权限不足
+}
